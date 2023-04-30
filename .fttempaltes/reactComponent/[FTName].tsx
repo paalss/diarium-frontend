a7d099c9d8cea5514467cfd0039eb0a6e3f69d23
paalss <paalss94@gmail.com>
2023-04-30 22:09:36 +0200

    chore: setup folder templates

---
+++ .fttempaltes/reactComponent/[FTName].tsx
@@ -0,0 +1,13 @@
+import classes from './[FTName].module.css';
+
+interface [FTName]Props {
+
+};
+
+const [FTName] = ({}: [FTName]Props) => {
+    return (
+        <div className={classes.root}></div>
+    )
+};
+
+export { [FTName] }
